3d9f496116bb73f93cfb88d803a7dd27d9f4d0b7
W. Joe Meese <meese022@umn.edu>
2022-06-10 16:45:30 -0500

    Add a type-hierarchy test

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,4 +23,16 @@ using Test
         output_string = "\nValues for ParametricParam{Int64}:\n{\n   \"x\": $inside\n}\n\n"
         output_string == pretty_display(String, ParametricParam(inside))
     end
+
+    # Test a struct that lives in a type hierarchy
+    @test let
+        @jsonable struct SubtypeParam <: Number
+            real_part::Int
+            imag_part::Float64
+        end
+        inside_real = 42
+        inside_imag = -42 # Converts the Float64 to an Int64 for string purposes I guess
+        output_string = "\nValues for SubtypeParam:\n{\n   \"real_part\": $inside_real,\n   \"imag_part\": $inside_imag\n}\n\n"
+        output_string == pretty_display(String, SubtypeParam(inside_real, inside_imag))
+    end
 end
